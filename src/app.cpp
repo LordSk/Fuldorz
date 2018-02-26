9cf3444148f5b73dfc5d9778fba7ecb6968a1133
LordSk <lordskelethom@gmail.com>
2018-02-26 10:55:24 +0100

    init

---
+++ src/app.cpp
@@ -0,0 +1,10 @@
+#ifndef UNICODE
+    #define UNICODE
+#endif
+#include <windows.h>
+#include <SDL2/SDL.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+    return 0;
+}
